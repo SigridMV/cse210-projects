e2767f85a9015ea276e06c5d13628a3520dd97d3
Sigrid Muller <sigrid.muller.vidal@gmail.com>
2023-04-19 16:40:39 -0400

    Learning Activity - C# Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,6 +5,43 @@ class Program
     static void Main(string[] args)
     {   
         // This is prep 2
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter your grade percentage: ");
+        string valueFromUser = Console.ReadLine();
+
+        int value = int.Parse(valueFromUser);
+
+        string letter = "";
+
+        if (value >= 90)
+        {
+            letter = "A";
+        }
+        else if (value >=80)
+        {
+            letter = "B";
+        }
+        else if (value >= 70)
+        {
+            letter = "C";
+        }
+        else if (value >=60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+
+        if (value >= 70)
+        {
+            Console.WriteLine("Well done, you passed!");
+        }
+        else
+        {
+            Console.WriteLine("You didn't pass this time, but keep trying and you'll get there.");
+        }
     }
 }
