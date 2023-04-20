15733c6b8e09ce555d807969b6bec46320b2b04e
Sigrid Muller <sigrid.muller.vidal@gmail.com>
2023-04-19 21:40:53 -0400

    Stretch Challenge

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,32 +1,44 @@
 using System;
 
-class Program{
-    static void Main(string[] args) {
-
-        Random randomGenerator = new Random();
-        int magicNumber = randomGenerator.Next(1, 101);
-
-        int guess = -1;
-        int numGuesses = 0;
-
-        while (guess != magicNumber)
+class Program
+{
+    static void Main(string[] args)
+    {
+        string response;
+        do
         {
-            Console.Write("What is the magic number? ");
-            guess = int.Parse(Console.ReadLine());
-            numGuesses++;
-            
-            if (magicNumber > guess)
+
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
+
+            int guess = -1;
+            int numGuesses = 0;
+
+
+            do
             {
-                Console.WriteLine("Higher");
-            }
-            else if (magicNumber < guess)
-            {
-                Console.WriteLine("Lower");
-            }
-            else
-            {
-                Console.WriteLine($"Congratulations!, you guessed it at {numGuesses}!");
-            }
-        }
+
+                Console.Write("What is the magic number? ");
+                guess = int.Parse(Console.ReadLine());
+                numGuesses++;
+
+                if (magicNumber > guess)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (magicNumber < guess)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine($"Congratulations!, you guessed it at {numGuesses}!");
+                }
+            } while (guess != magicNumber);
+
+            Console.Write("Do you want to continue? ");
+            response = Console.ReadLine();
+        } while (response == "yes");
+
     }
 }
