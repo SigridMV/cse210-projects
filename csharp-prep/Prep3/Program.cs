0502ec2d4e44bd048a2c07d803b06a05d9369f5f
Sigrid Muller <sigrid.muller.vidal@gmail.com>
2023-04-19 21:10:51 -0400

    Learning Activity - C# Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,32 @@
 using System;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
+class Program{
+    static void Main(string[] args) {
+
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+        int numGuesses = 0;
+
+        while (guess != magicNumber)
+        {
+            Console.Write("What is the magic number? ");
+            guess = int.Parse(Console.ReadLine());
+            numGuesses++;
+            
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine($"Congratulations!, you guessed it at {numGuesses}!");
+            }
+        }
     }
 }
